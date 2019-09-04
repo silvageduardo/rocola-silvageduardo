28c6bfb255fb066808b56f0a5e974a1d5e298a6a
edu <silvaeduardo1144@gmail.com>
2019-09-03 21:28:59 -0300

    tp1 con enum

--- src/main/java/ar/edu/unahur/obj2/tp1/Cancion.java
+++ src/main/java/ar/edu/unahur/obj2/tp1/Cancion.java
@@ -4,7 +4,12 @@ public class Cancion {
     private String titulo;
     private Float duracion;
     private String nombreAutor;
+    private Genero generos;
 
-
-
+    public Cancion(String titulo, Float duracion, String nombreAutor, Genero generos) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+        this.nombreAutor = nombreAutor;
+        this.generos = generos;
+    }
 }
--- src/main/java/ar/edu/unahur/obj2/tp1/TP1.java
+++ src/main/java/ar/edu/unahur/obj2/tp1/TP1.java
@@ -1,8 +1,12 @@
 package ar.edu.unahur.obj2.tp1;
 
+import static ar.edu.unahur.obj2.tp1.Genero.folk;
+
 public class TP1 {
 
     public static void main(String[] args) {
+        Cancion cancion = new Cancion("asd",1.2f,"dfg",folk);
+
         System.out.println("Rocola!");
     }
 }
